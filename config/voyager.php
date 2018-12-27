8bd0201b370f625849c4208d00dec42f6039f8e9
Margo <rita.yurtina@mail.ru>
2018-12-27 21:02:31 +0500

    Creating a Voyager Hook

--- config/voyager.php
+++ config/voyager.php
@@ -206,6 +206,7 @@ return [
     // Here you can specify additional assets you would like to be included in the master.blade
     'additional_css' => [
         //'css/custom.css',
+
     ],
 
     'additional_js' => [
