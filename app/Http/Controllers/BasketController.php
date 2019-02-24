f1de74486e2cd45889163edd9a003eb09ad1534a
Margo <rita.yurtina@mail.ru>
2019-02-24 21:27:34 +0500

    basket вывод данных

--- app/Http/Controllers/BasketController.php
+++ app/Http/Controllers/BasketController.php
@@ -4,21 +4,42 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Basket;
+use App\Product;
+use Illuminate\Support\Facades\DB;
 
 
 class BasketController extends Controller
 {
-    public function basket(Request $request){
+    public function basket(){
         $id_user = auth()->user()->id;
 //        $baskets = DB::table('baskets')->where('id', $id_user)->first();
 
-        $baskets = Basket::
-        select(['id','id_user','id_products','UnitPrice','quantity'])
-            ->where('id_user', $id_user)
-            ->pluck('id_user','id_products','UnitPrice','quantity');
-        dump($baskets);
+        //$users = DB::table('')->select('name', 'email')->get();
 
-        return view('representation/shopping_cart', ['baskets' => $baskets]);
+//        $baskets = Basket::
+//        select(['id','id_user','id_products','UnitPrice','quantity'])
+//            ->where('id_user', $id_user)
+//            ->get();
+        //dump($baskets);
+
+
+
+        $products = DB::table('products')
+            ->join('basket', 'products.id', '=', 'basket.id_products')
+            ->select('name','UnitPrice')
+            ->get();
+
+
+        $baskets = DB::table('basket')
+            ->where('id_user', $id_user)
+            ->join('products', 'basket.id_products','=','products.id' )
+            ->select('name','image','UnitPrice','id_user','id_products','quantity','final_price')
+            ->get();
+
+
+       // dump($baskets);
+
+        return view('representation/shopping_cart', ['baskets' => $baskets, 'products' => $products]);
 
 
     }
--- resources/views/representation/shopping_cart.blade.php
+++ resources/views/representation/shopping_cart.blade.php
@@ -31,30 +31,37 @@
                                         <th class="column-4">Quantity</th>
                                         <th class="column-5">Total</th>
                                     </tr>
-
+                                    @foreach ($baskets as $basket)
                                     <tr class="table_row">
                                         <td class="column-1">
                                             <div class="how-itemcart1">
-                                                <img src="images/item-cart-04.jpg" alt="IMG">
+                                                <img src="storage/{{ $basket->image}}" alt="IMG">
                                             </div>
                                         </td>
-                                        <td class="column-2">Fresh Strawberries</td>
-                                        <td class="column-3">$ 36.00</td>
+
+                                            <td class="column-2">
+                                                {{ $basket->name}}
+                                            </td>
+
+                                        <td class="column-3">{{ $basket->UnitPrice}}</td>
                                         <td class="column-4">
                                             <div class="wrap-num-product flex-w m-l-auto m-r-0">
                                                 <div class="btn-num-product-down cl8 hov-btn3 trans-04 flex-c-m">
                                                     <i class="fs-16 zmdi zmdi-minus"></i>
                                                 </div>
 
-                                                <input class="mtext-104 cl3 txt-center num-product" type="number" name="num-product1" value="1">
+                                                <input class="mtext-104 cl3 txt-center num-product" type="number" name="num-product1" value='{{ $basket->quantity}}'>
 
                                                 <div class="btn-num-product-up cl8 hov-btn3 trans-04 flex-c-m">
                                                     <i class="fs-16 zmdi zmdi-plus"></i>
                                                 </div>
                                             </div>
                                         </td>
-                                        <td class="column-5">$ 36.00</td>
+                                        <td class="column-5">₽
+                                            {{ $basket->final_price}}
+                                        </td>
                                     </tr>
+                                    @endforeach
 
                                     <tr class="table_row">
                                         <td class="column-1">
